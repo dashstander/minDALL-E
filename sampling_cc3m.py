94294576a0e45a5dfc017d9796c218762e2a0344
Dashiell Stander <dstander@protonmail.com>
2022-03-11 14:22:01 -0800

    fixed it up

--- sampling_cc3m.py
+++ sampling_cc3m.py
@@ -11,12 +11,19 @@ import polars as pl
 import clip
 import numpy as np
 from PIL import Image
+import re
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from dalle.models import Dalle
 from dalle.utils.utils import set_seed, clip_score
 
+
+def clean_text(text):
+    text = text.strip()
+    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r' ', '_', text)
+    return text
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--num_candidates', type=int, default=16)
@@ -37,9 +44,12 @@ device = 'cuda:0'
 model = Dalle.from_pretrained('minDALL-E/1.3B')  # This will automatically download the pretrained model.
 model.to(device=device)
 
-data = pl.read_parquet('index.parquet').filter(pl.col('group') == args.group)
+with open('prompts_specific.txt') as txtfile:
+    data = [line.strip() for line in txtfile.readlines()]
 
-for key, group, caption in data.records():
+#data = pl.read_parquet('index.parquet').filter(pl.col('group') == args.group)
+
+for i, caption in enumerate(data.records()):
     # Sampling
     images = model.sampling(
         prompt=caption,
@@ -64,11 +74,14 @@ for key, group, caption in data.records():
 
     # Save images
     images = images[rank]
+    clean_caption = clean_text(caption)
     print(rank, images.shape)
-    if not os.path.exists(f'./figures/{group}'):
-        os.makedirs(f'./figures/{group}', exist_ok=True)
-    for i in range(min(16, args.num_candidates)):
-        if i > 0:
+    if not os.path.exists(f'./figures/{clean_caption}'):
+        os.makedirs(f'./figures/{clean_caption}', exist_ok=True)
+    for j in range(min(16, args.num_candidates)):
+        if j > 0:
             break
         im = Image.fromarray((images[i]*255).astype(np.uint8))
-        im.save(f'./figures/{group}/{key}.png')
+        im.save(f'./figures/{clean_caption}/{i}.png')
+    with open(f'figures/{clean_caption}/{i}.txt') as txtfile:
+        txtfile.write(caption)
